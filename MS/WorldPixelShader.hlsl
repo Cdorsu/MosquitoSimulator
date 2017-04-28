e89df4ccced25d255c0c5a7c4062bb166f5125e2
Cdorsu <sdobrica188@gmail.com>
2017-04-28 16:27:07 +0300

    Fixed specular bug

--- MS/WorldPixelShader.hlsl
+++ MS/WorldPixelShader.hlsl
@@ -26,8 +26,8 @@ float4 GetSpecular(float lightIntensity, float specPower, float3 normal, float3 
         float3 PixelToCam, float4 specularColor, float4 specularIntensity)
 {
     float3 reflection = normalize(2 * lightIntensity * normal - lightDir);
-    float angle = dot(PixelToCam, lightDir);
-    float4 specular = saturate(pow(angle, specPower));
+    float angle = saturate(dot(PixelToCam, lightDir));
+    float4 specular = pow(angle, specPower);
     specular *= specularIntensity;
     return specular * specularColor;
 }
